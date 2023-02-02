f76f36accc68289c1877e29266101742eb3dee12
Victor <victormoraesvk@gmail.com>
2023-02-01 22:43:30 -0300

    Iniciando o projeto

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+	include_once("config/url.php");
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<title>Agenda de Contatos</title>
+ 	<!-- BOOTSTRAP -->
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ 	<!-- FONT AWESOME -->
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ 	<!-- CSS -->
+ 	<link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+ </head>
+ <body>
+ 	<h1>Teste</h1>
+ 	<i class="fas fa-eye"></i>
+ </body>
+ </html>
